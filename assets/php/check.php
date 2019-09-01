333e21c9a6073c7f980a7188c0ecfc1e6853e8e0
shunya_endoh <shunya.endoh@gmail.com>
2019-09-01 12:27:07 +0800

    登録ページの軽いレイアウト

---
+++ assets/php/check.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>check</title>
+</head>
+<body>
+    <div id="check-form">
+        <form action="./thanks.php" method="post" enctype="">
+        <dl class="check-form-content">
+            <dt>name:　</dt>
+            <dd></dd>
+
+            <dt>email address:　</dt>
+            <dd></dd>
+
+            <dt>password:　</dt>
+            <dd>**********</dd>
+
+            <dt>profile image:　</dt>
+            <dd></dd>
+        </dl>
+        <div><a href="../../index.html?action=rewrite">&laquo;&nbsp;rewrite</a> | <input type="submit" value="register"></div>
+
+        </form>
+
+    </div>
+    
+</body>
+</html>
---
+++ assets/php/thanks.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>thanks</title>
+</head>
+<body>
+    <div id="thanks">
+        <p>User registration is complete.</p>
+        <p><a href="../">sign in</a></p>
+    </div>
+</body>
+</html>
